122a5d30d05edb637f8c0927cf69018e98598902
wertylu <alfeziat@gmail.com>
2022-04-11 20:04:47 +0300

    Upgradedx3 Maven

--- src/main/java/milk/lab3/iot/Manager.java
+++ src/main/java/milk/lab3/iot/Manager.java
@@ -7,14 +7,19 @@ import java.util.stream.Collectors;
 public class Manager {
 public List<MilkProduct> sortByPrice (List<MilkProduct> milkbox, boolean descendingOrder){
     if(!descendingOrder){
+        System.out.println("in increasing order by price");
         return  milkbox.stream().sorted(Comparator.comparingDouble(MilkProduct::getPriceInUahPerUnit)).collect(Collectors.toList());
     }
+    System.out.println("in decreasing order by price");
     return  milkbox.stream().sorted(Comparator.comparing(MilkProduct::getPriceInUahPerUnit).reversed()).collect(Collectors.toList());
 }
 public List<MilkProduct> sortByOrigin (List<MilkProduct> milkbox, boolean descendingOrder){
     if(!descendingOrder){
+        System.out.println("in increasing order by origin");
         return  milkbox.stream().sorted(Comparator.comparing(MilkProduct::getOrigin)).collect(Collectors.toList());
     }
+    System.out.println("in decreasing order by origin");
     return  milkbox.stream().sorted(Comparator.comparing(MilkProduct::getOrigin).reversed()).collect(Collectors.toList());
+
 }
 }
